fe623d84641227ba4a8c83a796e707d0667594f5
Luan Ricardo <luanrnr1227@gmail.com>
2024-01-23 18:09:13 -0300

    introducao

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function menuShow() {
   let menuMobile = document.querySelector(".mobile-menu");
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
-    document.querySelector(".icon").src = "img/icones/menu_white_36dp.png";
+    document.querySelector(".icon").src = "img/icones/menu_white_36dp.svg";
   } else {
     menuMobile.classList.add("open");
     document.querySelector(".icon").src = "img/icones/close_white_36dp.svg";
