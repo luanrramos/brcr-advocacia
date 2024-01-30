c7a5b5c9c07c666807d810d711b5d8d9b6b486ee
Luan Ricardo <luanrnr1227@gmail.com>
2024-01-30 19:25:15 -0300

    Testes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,4 @@
 function show() {
   document.querySelector(".hamburger").classList.toggle("open");
   document.querySelector(".navigation").classList.toggle("active");
-  var body = document.querySelector("body");
-
-  body.classList.toggle("shadow");
 }
