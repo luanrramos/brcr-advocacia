1814e531b03005a52e4127706bbedec87e445889
Luan Ricardo <luanrnr1227@gmail.com>
2024-01-31 23:40:11 -0300

    feat (Adição do JS)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Função dedicada ao menu hambúrguer
+// Função - menu hambúrguer e sombra no body
 function show() {
   document.querySelector(".hamburger").classList.toggle("open");
   document.querySelector(".navigation").classList.toggle("active");
+  var body = document.querySelector("body");
+
+  body.classList.toggle("shadow");
+}
+
+//Troca de mapa no Footer
+var btn_cg = document.querySelector("input .btn-cg");
+var btn_es = document.querySelector("input .btn-es");
+function mapaEs() {
+  var mapa_cg = document.querySelector("a .mapa-cg");
+  var mapa_es = document.querySelector("a .mapa-es");
+
+  mapa_cg.style.display = "none";
+  mapa_es.classList.remove("inativo");
+}
+function mapaCg() {
+  var mapa_cg = document.querySelector("a .mapa-cg");
+  var mapa_es = document.querySelector("a .mapa-es");
+
+  mapa_cg.style.display = "block";
+  mapa_es.classList.add("inativo");
+}
+
+//Animação - Homepage
+if (window.SimpleAnime) {
+  new SimpleAnime();
 }
