be78151bfbef792b9b2c0d40e5c7a19e9e71cea8
Luan Ricardo <luanrnr1227@gmail.com>
2024-03-04 19:08:16 -0300

    Update - subtitulos em caixa alta e subtítulos em caixa baixa / Adição da página políticas de privacidade e termos da empresa

---
+++ js/termos.js
@@ -0,0 +1,18 @@
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+  console.log(pergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
